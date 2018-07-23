238b3337d0141c97b7d2a4713bc154523e308012
chinory <32269068+chinory@users.noreply.github.com>
2018-07-23 15:26:42 +0800

    support extra keys distinguish files

---
+++ dev/MapDeepIterator.js
@@ -0,0 +1,18 @@
+function * MapDeepIterator (map, maxDepth) {
+  if (maxDepth < 1) {
+    yield map
+    return
+  }
+  for (const stack = [map.values()]; stack.length > 0;) {
+    const result = stack[stack.length - 1].next()
+    if (result.done) {
+      stack.pop()
+    } else if (stack.length < maxDepth && result.value instanceof Map) {
+      stack.push(result.value.values())
+    } else {
+      yield result.value
+    }
+  }
+}
+
+module.exports = MapDeepIterator
---
+++ dev/MultikeyMap.js
@@ -0,0 +1,74 @@
+class MultikeyMap {
+  set (keys, value) {
+    if (keys.length > 0) {
+      let map = this.hasOwnProperty('entry') ? this.entry : this.entry = new Map()
+      const last = keys.length - 1
+      for (let i = 0; i < last; ++i) {
+        if (map.has(keys[i])) {
+          map = map.get(keys[i])
+        } else {
+          const next = new Map()
+          map.set(keys[i], next)
+          map = next
+        }
+      }
+      map.set(keys[last], value)
+    } else {
+      this.value = value
+    }
+    return this
+  }
+  get (keys) {
+    if (keys.length > 0) {
+      if (this.hasOwnProperty('entry')) {
+        let map = this.entry
+        const last = keys.length - 1
+        for (let i = 0; i < last; ++i) {
+          if (map.has(keys[i])) {
+            map = map.get(keys[i])
+          } else {
+            return
+          }
+        }
+        return map.get(keys[last])
+      }
+    } else {
+      return this.value
+    }
+  }
+  * values () { // bug: if value is Map, will iter it too
+    if (this.hasOwnProperty('value')) {
+      yield this.value
+    }
+    if (this.hasOwnProperty('entry')) {
+      for (const stack = [this.entry.values()]; stack.length > 0;) {
+        const result = stack[stack.length - 1].next()
+        if (result.done) {
+          stack.pop()
+        } else if (result.value instanceof Map) {
+          stack.push(result.value.values())
+        } else {
+          yield result.value
+        }
+      }
+    }
+  }
+}
+
+function * MapDeepIterator (map, maxDepth) {
+  if (maxDepth < 1) return map
+  for (const stack = [map.values()]; stack.length > 0;) {
+    const result = stack[stack.length - 1].next()
+    if (result.done) {
+      stack.pop()
+    } else if (stack.length < maxDepth && result.value instanceof Map) {
+      stack.push(result.value.values())
+    } else {
+      yield result.value
+    }
+  }
+}
+
+module.exports = {
+  MultikeyMap, MapDeepIterator
+}
